8b3f5b64a5eb96146809b354f0c229bff480d6bd
yby1213 <2320026212@qq.com>
2024-10-16 10:11:57 +0800

    init

---
+++ src/main/java/com/seata/test/solveseata/SolveseataApplication.java
@@ -0,0 +1,13 @@
+package com.seata.test.solveseata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolveseataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolveseataApplication.class, args);
+    }
+
+}
